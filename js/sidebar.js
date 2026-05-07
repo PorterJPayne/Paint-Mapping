f2cd71eaac8429a90f49b4c2cd77c89a92afffeb
PorterJPayne <porterpayne04@gmail.com>
2026-05-07 02:49:59 -0600

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,1 +1,36 @@
+function renderSidebar(room){
 
+  paintContainer.innerHTML = "";
+
+  room.paints.forEach(paint=>{
+
+    const card =
+      document.createElement("div");
+
+    card.className =
+      "paint-card";
+
+    card.innerHTML = `
+      <div class="paint-surface">
+        ${paint.surface || ""}
+      </div>
+
+      <div class="paint-name">
+        ${paint.color || ""}
+      </div>
+
+      <div class="paint-code">
+        ${paint.code || ""}
+      </div>
+
+      <div>
+        Kiln Spec:
+        ${paint.kilnSpec || ""}
+      </div>
+    `;
+
+    paintContainer.appendChild(card);
+
+  });
+
+}
