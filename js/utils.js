e564adec59a0241c7c476b55b0dc825f038b3269
PorterJPayne <porterpayne04@gmail.com>
2026-05-07 03:52:10 -0600

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,39 @@
-//pp
+function getFloor(){
+
+  return buildingData.floors[currentFloor];
+
+}
+
+function getCurrentRoom(){
+
+  return getFloor().rooms.find(
+    r => r.id === currentRoom
+  );
+
+}
+
+function getPaintLibrary(){
+
+  const map = new Map();
+
+  Object.values(buildingData.floors)
+    .forEach(floor=>{
+
+      floor.rooms.forEach(room=>{
+
+        room.paints.forEach(paint=>{
+
+          const key =
+            JSON.stringify(paint);
+
+          map.set(key,paint);
+
+        });
+
+      });
+
+    });
+
+  return [...map.values()];
+
+}
