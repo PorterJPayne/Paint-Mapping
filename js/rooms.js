dd9abff1070db3d1983a8a8a743ab7bba260caa2
PorterJPayne <porterpayne04@gmail.com>
2026-05-07 04:30:02 -0600

    Update rooms.js

--- js/rooms.js
+++ js/rooms.js
@@ -1,1 +1,61 @@
-//ff
+function renderRoomList(){
+
+  roomList.innerHTML = "";
+
+  getFloor().rooms.forEach(room=>{
+
+    const div =
+      document.createElement("div");
+
+    div.className =
+      "room-item";
+
+    if(room.id === currentRoom){
+
+      div.classList.add("active");
+
+    }
+
+    div.textContent =
+      room.id;
+
+    div.onclick = ()=>{
+
+      selectRoom(room.id);
+
+    };
+
+    roomList.appendChild(div);
+
+  });
+
+}
+
+function selectRoom(id){
+
+  currentRoom = id;
+
+  const room =
+    getCurrentRoom();
+
+  if(!room) return;
+
+  emptyState.classList.add(
+    "hidden"
+  );
+
+  roomPanel.classList.remove(
+    "hidden"
+  );
+
+  roomTitle.textContent =
+    room.id;
+
+  floorLabel.textContent =
+    currentFloor;
+
+  renderSidebar(room);
+
+  renderFloor();
+
+}
